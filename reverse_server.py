cce6c40cccf76020e09c3e81ac2c63b216393564
Jack-lang-creator <63970626+Jack-lang-creator@users.noreply.github.com>
2020-04-19 23:12:39 +0530

    Create reverse_server.py

---
+++ reverse_server.py
@@ -0,0 +1,30 @@
+#!/bin/env/python
+import socket
+#configure server host
+serv_host = "127.0.0.1"
+#configure server port
+serv_port = 5001
+buffer = 1024
+s = socket.socket()
+#bind the ip with port
+s.bind((serv_host,serv_port))
+#listen for connections
+s.listen(5)
+print(f"Listening as {serv_host}:{serv_port} ...")
+client_socket = s.accept()
+client_addr = s.accept()
+#message = "hello World".encode()
+#client_socket.send(message)
+print(f"{client_addr[0]}:{client_addr[1]} Connected!")
+while True:
+   command = input("Enter the command: ")
+   client_socket.send(command).encode()
+   if command == "exit":
+      break
+   result = client_socket.recv(buffer).decode()
+   print(result)
+#close client connections   
+client_socket.close()
+#close server connection
+s.close()
+
